6a5415790fd94ad7a5f899c8c31f2005e12806c2
Bjørn Madsen <bm@aeons.dk>
2014-05-21 14:24:32 +0200

    Add grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+  // Load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    shell: {
+      jekyll: {
+        command: 'rm -rf _site; bundle exec jekyll build',
+        stdout: true
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      jekyllSources: {
+        files: [
+          '*.html', '*.yml', '_assets/javascripts/**.js', '_posts/**',
+          'pages/**', '_data/**', '_hold/**', '_includes/**', '_layouts/**',
+          '**/*.md'
+        ],
+        tasks: ['shell:jekyll']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          base: '_site/',
+          port: 4000
+        }
+      }
+    },
+
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.server.options.port %>/'
+      }
+    }
+  });
+
+  grunt.registerTask('server', [
+    'connect:server',
+    'shell:jekyll',
+    'open:server',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', 'server');
+};
